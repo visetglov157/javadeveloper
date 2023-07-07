5dd6f35b0b222d542b2a4fce9017b011fc55d022
Vladislav <vladislav33lavrov@gmail.com>
2023-07-07 20:57:10 +0300

    exercise1 is ready

--- homework_1/exercise1.java
+++ homework_1/exercise1.java
@@ -1,1 +1,17 @@
+package homework_1;
+import java.util.Scanner;
 
+public class exercise1 {
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите первое число: ");
+        int i = iScanner.nextInt();
+        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
+        iScanner.close();
+    }
+
+    public static int giveMeNumber(int a) {
+        return (a * (a + 1)) / 2;
+    }
+}
