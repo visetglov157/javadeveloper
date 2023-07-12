eacf3e07ba6c2254b718971579b0501594c914cf
visetglov157 <vladislav33lavrov@gmail.com>
2023-07-12 13:04:22 +0200

    exercise1 is ready

--- homework_3/exercise1.java
+++ homework_3/exercise1.java
@@ -1,1 +1,32 @@
+package homework_3;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
 
+public class exercise1{
+    public static void main(String[] args) throws Exception {
+        System.out.print("Введите длину списка N: ");
+        try (Scanner iScanner = new Scanner(System.in)) {
+            int N = iScanner.nextInt();
+            ArrayList<Integer> ar = new ArrayList<Integer>();
+            Random ran = new Random();
+            for (int i = 0; i < N; i++) {
+                int x = ran.nextInt(N);
+                ar.add(x);
+            }
+            System.out.println("Произвольный список целых чисел: \n " + ar);
+
+            for (int i = ar.size() - 1; i >= 0; i--) {
+                int even = ar.get(i) % 2;
+                if (even == 0) {
+                    System.out.println("Четное число:" + ar.get(i));
+                    ar.remove(i);
+                }
+            }
+            Collections.sort(ar);
+            System.out.println("Список с удаленными четными числами: \n " + ar);
+        }
+    }
+
+}
